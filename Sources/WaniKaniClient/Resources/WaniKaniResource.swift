e7decae2a1f884d540e691f7bcf97c1354724b1c
Chris Laverty <cplaverty@users.noreply.github.com>
2023-09-20 17:23:19 +0100

    Add data property to WaniKaniResource protocol

--- Sources/WaniKaniClient/Resources/WaniKaniResource.swift
+++ Sources/WaniKaniClient/Resources/WaniKaniResource.swift
@@ -1,10 +1,14 @@
 import Foundation
 
 public protocol WaniKaniResource: Codable {
+    associatedtype ResourceData
+    
     /// The kind of object returned.
     var objectType: ResourceObjectType { get }
     /// The URL of the request.
     var url: URL { get }
     /// The last time that particular resource was updated.
     var dataUpdatedAt: Date { get }
+    /// The attributes that are specific to the kind of resource.
+    var data: ResourceData { get }
 }
